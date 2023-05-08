863d1826d450b3ffd1590b79eacaaff253d3e82b
liusichao <985113065@qq.com>
2023-05-08 17:07:15 +0800

    Add Embedded

---
+++ Embedded/STDC/02day/demo/07var/07var.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main () {
+int val=1,val1=9;//声明整型变量val与val1，并加以初始化。
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+val=val+1;//根据val变量中原有的内容＋1，得到下一个数字，并记录到val变量里。
+val1=val1-1;
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+val=val+1;  //根据val变量中原有的内容＋1，得到下一个数字，并记录到val变量里。
+val1=val1-1;  //根据val1变量中原有的内容-1，得到下一个数字，并记录到val1变量里。
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+val=val+1;  //根据val变量中原有的内容＋1，得到下一个数字，并记录到val变量里。
+val1=val1-1;  //根据val1变量中原有的内容-1，得到下一个数字，并记录到val1变量里。
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+val=val+1;  //根据val变量中原有的内容＋1，得到下一个数字，并记录到val变量里。
+val1=val1-1;  //根据val1变量中原有的内容-1，得到下一个数字，并记录到val1变量里。
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+val=val+1;  //根据val变量中原有的内容＋1，得到下一个数字，并记录到val变量里。
+val1=val1-1;  //根据val1变量中原有的内容-1，得到下一个数字，并记录到val1变量里。
+printf("%d*%d=%d\n", val,val1,val*val1);
+
+return 0;
+}
+
